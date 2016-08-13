b39705497251b99d8ffa8eaea86fe2efc6afb2b0
manognaniit <vvrlmanogna@gmail.com>
2016-08-13 22:52:32 +0530

    Add files via upload

---
+++ shoppingcart/src/main/java/com/niit/shoppingcart/dao/UserDetailsDAOImpl.java
@@ -0,0 +1,76 @@
+package com.niit.shoppingcart.dao;
+
+
+import java.util.List;
+
+import org.hibernate.HibernateException;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.niit.shoppingcart.model.UserDetails;
+
+@Repository(value="UserDetailsDAO")
+public class UserDetailsDAOImpl implements UserDetailsDAO{
+	@Autowired
+	private SessionFactory sessionFactory;
+	public UserDetailsDAOImpl(SessionFactory sessionFactory)
+	{
+		this.sessionFactory=sessionFactory;
+	}
+@Transactional
+public boolean save(UserDetails userDetails){
+	try {
+		sessionFactory.getCurrentSession().save(userDetails);
+		return true;
+	} catch (HibernateException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+		return false;}
+	}
+	@Transactional
+	public boolean update(UserDetails userDetails){
+		try {
+			sessionFactory.getCurrentSession().update(userDetails);
+			return true;
+		} catch (HibernateException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return false;}
+		}
+		@Transactional
+		public boolean delete(UserDetails userDetails){
+			try {
+				sessionFactory.getCurrentSession().delete(userDetails);
+				return true;
+			} catch (HibernateException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				return false;
+			}
+}
+
+public UserDetails get(String id)
+{
+	String hql="from UserDetails where id="+"'"+id+"'";
+			Query query=sessionFactory.getCurrentSession().createQuery(hql);
+			List<UserDetails> list=query.list();
+			if(list==null){
+				return null;
+			}
+			else{
+				return list.get(0);
+				
+			}
+}
+public	List<UserDetails> list(){
+	String hql="from UserDetails";
+	Query query=sessionFactory.getCurrentSession().createQuery(hql);
+	return query.list();
+		
+	}
+}
+
+
